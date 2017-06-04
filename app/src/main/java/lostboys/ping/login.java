f4f1ca2ae539aa56fdb32baca8e67720a6a85866
lithiumlkid <luvnenvy@gmail.com>
2017-06-04 16:49:31 +0800

    First

---
+++ app/src/main/java/lostboys/ping/login.java
@@ -0,0 +1,13 @@
+package lostboys.ping;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class login extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}

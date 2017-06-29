ca4c6db5239bf0aa38e0dd8b7d4c3c60bfb4142e
lithiumlkid <luvnenvy@gmail.com>
2017-06-29 14:07:28 +0800

    added database test and time picker

--- app/src/main/java/lostboys/ping/EventCreate.java
+++ app/src/main/java/lostboys/ping/EventCreate.java
@@ -1,21 +1,57 @@
 package lostboys.ping;
 
+import android.app.DialogFragment;
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
+import android.support.annotation.NonNull;
+import android.support.v4.app.FragmentActivity;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
 
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import lostboys.ping.Models.EventEntry;
+import lostboys.ping.Pickers.TimePickerFragment;
 
 
 /**
  * Created by Syafiq on 23/6/2017.
  */
 
-public class EventCreate extends AppCompatActivity {
+public class EventCreate extends FragmentActivity {
     private DatabaseReference mDatabase;
+    private DatabaseReference eventCloudEndPoint;
+    private FirebaseAuth mFirebaseAuth;
+    private FirebaseUser mFirebaseUser;
+    private EditText name;
+    private EditText date;
+    private EditText time;
 
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_createevent);
+        mFirebaseAuth = FirebaseAuth.getInstance();
+        mFirebaseUser = mFirebaseAuth.getCurrentUser();
+        mDatabase =  FirebaseDatabase.getInstance().getReference("users");
+        eventCloudEndPoint =  mDatabase.child(
+                mFirebaseUser.getUid()).child("events");
+        EventEntry event = new EventEntry("test2",10.0);
+        eventCloudEndPoint.setValue(event).addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e) {
+                Log.d("event", e.getLocalizedMessage());
+            }
+        });
 
     }
+
+    public void showTimePickerDialog(View v) {
+        DialogFragment newFragment = new TimePickerFragment();
+        newFragment.show(getFragmentManager(), "timePicker");
+    }
+
 }
---
+++ app/src/main/java/lostboys/ping/Models/EventEntry.java
@@ -0,0 +1,20 @@
+package lostboys.ping.Models;
+
+/**
+ * Created by Syafiq on 28/6/2017.
+ */
+
+public class EventEntry {
+
+    public String name;
+    public double lat;
+
+
+    public EventEntry(){}
+
+    public EventEntry(String name, double lat){
+        this.name=name;
+        this.lat=lat;
+    }
+
+}
